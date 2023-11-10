674d3b39f9f982b5303111ddfedc8eda73c8b700
Kvirike kemularia <kvirikekemularia@gmail.com>
2023-11-10 17:45:28 +0400

    building constructor
    using get/set

---
+++ index.js
@@ -0,0 +1,18 @@
+// Fahrenheit/Celsius converter
+class Thermostat{
+    constructor(thermos){
+      this.fahrenheit= thermos;
+    }
+    get temperature(){
+      return (5 / 9) * (this.fahrenheit - 32);
+    }
+    set temperature(celsius){
+      return this.fahrenheit = celsius * 9.0 / 5 + 32;
+    }
+  }
+  
+  
+  const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+  let temp = thermos.temperature;
+  console.log(temp);
+  
