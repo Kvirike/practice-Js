d3ffe63ff0f72758273e6f1aa63a5ded1b0f9031
Kvirike kemularia <kvirikekemularia@gmail.com>
2023-11-25 21:51:13 +0400

    multiplying numbers
    multiplying all numbers from nested arrays

--- index.js
+++ index.js
@@ -75,6 +75,18 @@ function alphabeticalOrder(arr) {
 }
 console.log(sum(1, 2, 5));
 
+// multiplying all numbers from nested arrays
+function multiplyAll(arr) {
+  let product = 1;
+for (let i = 0; i < arr.length; i++){
+  for(let j = 0; j < arr[i].length; j++){
+    product = product * arr[i][j]
+  }
+}
+  return product;
+}
+console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
+
 
 // returning first number from array, which returns true for passed function
  function findElement(arr, func) {
