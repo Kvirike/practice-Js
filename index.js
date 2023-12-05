076fb9323a946018c10067cbe961e8eaad4ca012
Kvirike kemularia <kvirikekemularia@gmail.com>
2023-12-05 17:33:56 +0400

    blackjack strategy
    playing safe with low cards. function tells whether to hold or bet.

--- index.js
+++ index.js
@@ -301,3 +301,35 @@ function frankenSplice(arr1, arr2, n) {
   }
   var data = lookUpProfile("Akira", "likes");
   console.log(data);
+
+
+  //blackjack strategy
+  let count = 0;
+  function cc(card) {
+  switch(card){
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count ++;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count --;
+      break;
+  }
+  
+  let holdbet = "Hold"
+  if(count > 0){
+    holdbet = "Bet"
+  }
+  
+  return count + " " + holdbet
+  }
+  
+  console.log(cc(2)); cc(3); cc(7); cc('K'); cc('A');
+  console.log(cc('A'));
