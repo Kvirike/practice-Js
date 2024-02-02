71d74f3bd7e987f274530d6a0a983714b2b1be08
Kvirike kemularia <kvirikekemularia@gmail.com>
2024-02-02 20:20:51 +0400

    reduce
    using reduce for detecting voters

--- index.js
+++ index.js
@@ -349,6 +349,32 @@ console.log(ofAge([
     { name: "Bob Ziroll", age: 100 }
 ]));
 
+
+//detecting voters
+function totalVotes(arr) {
+  return arr.reduce(function (acc, voter) {
+      return acc + (voter.voted ? 1 : 0);
+  }, 0);
+}
+
+var voters = [
+  {name:'Bob' , age: 30, voted: true},
+  {name:'Jake' , age: 32, voted: true},
+  {name:'Kate' , age: 25, voted: false},
+  {name:'Sam' , age: 20, voted: false},
+  {name:'Phil' , age: 21, voted: true},
+  {name:'Ed' , age:55, voted:true},
+  {name:'Tami' , age: 54, voted:true},
+  {name: 'Mary', age: 31, voted: false},
+  {name: 'Becky', age: 43, voted: false},
+  {name: 'Joey', age: 41, voted: true},
+  {name: 'Jeff', age: 30, voted: true},
+  {name: 'Zack', age: 19, voted: false}
+];
+
+var totalVoted = totalVotes(voters);
+console.log("Total number of people who voted:", totalVoted);
+
   //blackjack strategy
   let count = 0;
   function cc(card) {
